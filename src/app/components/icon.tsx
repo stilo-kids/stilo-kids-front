ef8bf04798e3ba34140f6e4c62191c94c85820f5
veraxqy <elderbruno2014@gmail.com>
2025-10-11 17:31:07 -0300

    config: instalação e configuração da biblioteca react-native-svg; criação do component Icon

---
+++ src/app/components/icon.tsx
@@ -0,0 +1,137 @@
+import React from "react";
+import ArrowLeft from "../assets/icons/ARROW-LEFT.svg";
+import ArrowRight from "../assets/icons/ARROW-RIGHT.svg";
+import Chart from "../assets/icons/CHART.svg";
+import Edit from "../assets/icons/EDIT.svg";
+import Eye from "../assets/icons/EYE.svg";
+import Help from "../assets/icons/HELP.svg";
+import Home from "../assets/icons/HOME.svg";
+import Money from "../assets/icons/MONEY.svg";
+import Package from "../assets/icons/PACKAGE.svg";
+import PieChart from "../assets/icons/PIE-CHART.svg";
+import Plus from "../assets/icons/PLUS.svg";
+import Settings from "../assets/icons/SETTINGS.svg";
+import Shirt from "../assets/icons/SHIRT.svg";
+import Trash from "../assets/icons/TRASH.svg";
+import Truck from "../assets/icons/TRUCK.svg";
+import { View } from "react-native";
+
+export const ArrowLeftIcon = () => {
+    return (
+        <View>
+            <ArrowLeft width={36} height={36} />
+        </View>
+    )
+}
+
+export const ArrowRightIcon = () => {
+    return (
+        <View>
+            <ArrowRight width={36} height={36} />
+        </View>
+    )
+}
+
+export const ChartIcon = () => {
+    return (
+        <View>
+            <Chart width={36} height={36} />
+        </View>
+    )
+}
+
+export const EditIcon = () => {
+    return (
+        <View>
+            <Edit width={36} height={36} />
+        </View>
+    )
+}
+
+export const EyeIcon = () => {
+    return (
+        <View>
+            <Eye width={36} height={36} />
+        </View>
+    )
+}
+
+export const HelpIcon = () => {
+    return (
+        <View>
+            <Help width={36} height={36} />
+        </View>
+    )
+}
+
+export const HomeIcon = () => {
+    return (
+        <View>
+            <Home width={36} height={36} />
+        </View>
+    )
+}
+
+export const MoneyIcon = () => {
+    return (
+        <View>
+            <Money width={36} height={36} />
+        </View>
+    )
+}
+
+export const PackageIcon = () => {
+    return (
+        <View>
+            <Package width={36} height={36} />
+        </View>
+    )
+}
+
+export const PieChartIcon = () => {
+    return (
+        <View>
+            <PieChart width={36} height={36} />
+        </View>
+    )
+}
+
+export const PlusIcon = () => {
+    return (
+        <View>
+            <Plus width={36} height={36} />
+        </View>
+    )
+}
+
+export const SettingsIcon = () => {
+    return (
+        <View>
+            <Settings width={36} height={36} />
+        </View>
+    )
+}
+
+export const ShirtIcon = () => {
+    return (
+        <View>
+            <Shirt width={36} height={36} />
+        </View>
+    )
+}
+
+export const TrashIcon = () => {
+    return (
+        <View>
+            <Trash width={36} height={36} />
+        </View>
+    )
+}
+
+export const TruckIcon = () => {
+    return (
+        <View>
+            <Truck width={36} height={36} />
+        </View>
+    )
+}
