da235e7818ae8caa321c1f56b2533ec8a5ac5933
veraxqy <elderbruno2014@gmail.com>
2025-10-11 16:24:30 -0300

    config: criação do component button

---
+++ src/app/components/button.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { colors } from "../../theme/colors";
+
+type Props = TouchableOpacityProps & {
+    children: React.ReactNode;
+};
+
+export const SimpleButton = ({ children, style, onPress, ...rest}: Props) => {
+    return (
+        <TouchableOpacity style={styles.simpleButton} onPress={onPress} {...rest}>
+            {children}
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    simpleButton: {
+        backgroundColor: colors.AZUL,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 10,
+        alignSelf: "flex-start",
+    },
+    iconButton: {
+    }
+})
