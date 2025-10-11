94c3a4df40dd196d5ff0386d63c70884281bddb1
veraxqy <elderbruno2014@gmail.com>
2025-10-11 09:29:44 -0300

    config: instalando fonte poppins através do google fontes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import '../../global.css';
+import { useFonts, Poppins_400Regular, Poppins_600SemiBold } from "@expo-google-fonts/poppins";
 
 export default function RootLayout() {
     return <Stack />;
