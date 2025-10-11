737ec4d84c43da3fc546b53375f47ee9e38c044c
veraxqy <elderbruno2014@gmail.com>
2025-10-11 09:21:25 -0300

    config: adicionando as cores do projeto como constantes no tailwind css

---
+++ src/theme/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  LARANJA: '#FBA53E',
+  AZUL: '#6DC4C5',
+  ROSA: '#F29DA6',
+  VERDE: '#85E085',
+  BRANCO: '#F0FEFB',
+  CINZA: '#CBE6E1',
+  PRETO: '#2E3332',
+  VERMELHO: '#FB3E3E',
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
+import { colors } from '.src/theme/colors';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        laranja: colors.LARANJA,
+        azul: colors.AZUL,
+        rosa: colors.ROSA,
+        verde: colors.VERDE,
+        branco: colors.BRANCO,
+        cinza: colors.CINZA,
+        preto: colors.PRETO,
+        vermelho: colors.VERMELHO,
+      }
+    },
   },
   plugins: [],
 }
