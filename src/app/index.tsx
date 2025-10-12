0ed59bfcc01cf0b9840c1a2d83a8ccfdeef26453
veraxqy <elderbruno2014@gmail.com>
2025-10-11 23:11:25 -0300

    config: criando o componente Card

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,10 @@
 import { View, Text } from "react-native";
 import '../../global.css';
-import { Button, buttonTextVariants } from "./components/Button";
 
 export default function Index() {
     return (
         <View>
             <Text>Stilo Kids!</Text>
-            <Button label="Botão"/>
         </View>
     );
 }
