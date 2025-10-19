3b11a8f017622e3176c0402ddc9666068284fab4
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-10-19 10:28:16 -0300

    fix: uso do tw na tela index de produtos

--- src/app/(tabs)/product.tsx
+++ src/app/(tabs)/product.tsx
@@ -13,14 +13,14 @@ export default function Index() {
     return (
         <View style={tw`flex-1 gap-5 flex-col items-center mt-8`}>
             <View>
-                <Button variant="cardButton" className="self-start h-auto">
-                    <Card className="flex-row justify-between items-center py-5 border-blue">
+                <Button variant="cardButton" style={tw`self-start h-auto`}>
+                    <Card style={tw`flex-row justify-between items-center py-5 border-blue`}>
                         <Text style={tw`font-semibold`}>Relatórios</Text>
                         <ArrowRightIcon color="black"/>
                     </Card>
                 </Button> 
             </View>
-            <Button onPress={() => router.push("/(stack)/(products)")} className="px-10" label="Adicionar novo"/>
+            <Button onPress={() => router.push("/(stack)/(products)")} style={tw`px-10`} label="Adicionar novo"/>
         </View>
     );
 }
