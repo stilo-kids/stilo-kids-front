93f4be732d39bc53c44d7b9fdeac6cc101d8988d
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-10-24 18:26:24 -0300

    feat: tipos do módulo de produtos

--- src/app/(stack)/(products)/types.ts
+++ src/app/(stack)/(products)/types.ts
@@ -18,17 +18,38 @@ export interface ProductSupplier {
     name: string;
 }
 
+// DTO de fornecedor
+export interface Supplier {
+    id: number;
+    name: string;
+}
+
 export interface Product {
     id: number;
     name: string;
     sale_price: number;
     cost_price: number;
     description: string;
-    suppliers: Array<ProductSupplier>;
+    suppliers: Array<Supplier>;
     brand: Brand;
     clothing_type: ClothingType;
     clothing_size: ClothingSize;
+    acquisition_date: string | Date | null;
 }
+
+export interface ProductCreateDTO {
+    name: string;
+    clothing_size_id: number;
+    clothing_type_id: number;
+    brand_id: number;
+    supplier_id: number;
+    description: string;
+    acquisition_date: Date | string | null;
+    sale_price: number;
+    cost_price: number;
+}
+
+
 
 export const tableHeaders = [
     '#',
