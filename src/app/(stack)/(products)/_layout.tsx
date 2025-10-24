17ae0dbb9db5a4934897f299e856b370dac4e288
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-10-24 18:26:36 -0300

    feat: layouts para o módulo de produtos

--- src/app/(stack)/(products)/_layout.tsx
+++ src/app/(stack)/(products)/_layout.tsx
@@ -1,9 +1,29 @@
 import { Stack } from "expo-router";
+import CustomStackHeader from "../../../components/CustomStackHeader";
+import { View } from "react-native";
 
-export default function SupplierLayout() {
-    return(
+export default function ProductLayout() {
+    return (
         <Stack>
-            <Stack.Screen name="products"/>
+            <Stack.Screen
+                options={{
+                    header: () => <CustomStackHeader title="Produtos" onHelpPress={() => ''} bgColor="orange" />,
+                }}
+                name="index"
+            />
+            <Stack.Screen
+                options={{
+                    header: () => <CustomStackHeader title="Produto" onHelpPress={() => ''} bgColor="orange" />,
+                }}
+                name="[id]"
+            />
+            <Stack.Screen
+                options={{
+                    header: () => <CustomStackHeader title="Editar Produto" onHelpPress={() => ''} bgColor="orange" />,
+                }}
+                name="edit/[id]"
+            />
+
         </Stack>
     );
 }
--- src/app/(stack)/_layout.tsx
+++ src/app/(stack)/_layout.tsx
@@ -4,6 +4,7 @@ export default function StackLayout() {
     return(
         <Stack screenOptions={{ headerShown: false}}>
             <Stack.Screen name="(suppliers)"/>
+            <Stack.Screen name="(products)"/>
         </Stack>
     );
 }
