19cb6155ee9d9e327de2bf6c33462ee687807eae
Mosiah Adam <mosiah.adam@escolar.ifrn.edu.br>
2025-10-24 18:24:21 -0300

    config: configuração do axios

--- api.ts
+++ api.ts
@@ -1,10 +1,13 @@
-import { axios } from "axios";
-import * as dotenv from "dotenv";
+import axios, { AxiosInstance } from "axios";
+import { Platform } from "react-native";
+import { API_URL } from "@env";
 
-dotenv.config();
+const BASE_URL =
+  Platform.OS === "android" ? "http://10.0.2.2:8000" : API_URL;
 
-const api = axios.create({
-    baseURL: process.env.API_URL,
+const api: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
 });
 
 export default api;
+export { BASE_URL };
